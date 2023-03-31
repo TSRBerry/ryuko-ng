e5dd6d62a63554a486b04f2ddd6b34c0f8acf558
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-03-31 16:26:13 +0200

    macro: Add aliases for macros

--- robocop_ng/cogs/macro.py
+++ robocop_ng/cogs/macro.py
@@ -10,7 +10,10 @@ from robocop_ng.helpers.macros import (
     add_macro,
     edit_macro,
     remove_macro,
-    get_macros,
+    get_macros_dict,
+    add_aliases,
+    remove_aliases,
+    clear_aliases,
 )
 
 
@@ -43,12 +46,36 @@ class Macro(Cog):
             await ctx.send(f"Error: Macro '{key}' already exists.")
 
     @commands.check(check_if_staff)
+    @commands.command(name="aliasadd", aliases=["addalias", "add_alias"])
+    async def add_alias_macro(self, ctx: Context, existing_key: str, *new_keys: str):
+        if len(new_keys) == 0:
+            await ctx.send("Error: You need to add at least one alias.")
+        else:
+            if add_aliases(existing_key, list(new_keys)):
+                await ctx.send(f"Added {len(new_keys)} aliases to macro '{existing_key}'!")
+            else:
+                await ctx.send(f"Error: No new and unique aliases found.")
+
+    @commands.check(check_if_staff)
     @commands.command(name="macroedit", aliases=["me", "editmacro", "edit_macro"])
     async def edit_macro(self, ctx: Context, key: str, *, text: str):
         if edit_macro(key, text):
             await ctx.send(f"Macro '{key}' edited!")
         else:
             await ctx.send(f"Error: Macro '{key}' not found.")
+
+    @commands.check(check_if_staff)
+    @commands.command(name="aliasremove", aliases=[
+        "aliasdelete", "delalias", "aliasdel", "removealias", "remove_alias", "delete_alias"
+    ])
+    async def remove_alias_macro(self, ctx: Context, existing_key: str, *remove_keys: str):
+        if len(remove_keys) == 0:
+            await ctx.send("Error: You need to remove at least one alias.")
+        else:
+            if remove_aliases(existing_key, list(remove_keys)):
+                await ctx.send(f"Removed {len(remove_keys)} aliases from macro '{existing_key}'!")
+            else:
+                await ctx.send(f"Error: None of the specified aliases were found for macro '{existing_key}'.")
 
     @commands.check(check_if_staff)
     @commands.command(
@@ -69,10 +96,18 @@ class Macro(Cog):
         else:
             await ctx.send(f"Error: Macro '{key}' not found.")
 
+    @commands.check(check_if_staff)
+    @commands.command(name="aliasclear", aliases=["clearalias", "clear_alias"])
+    async def clear_alias_macro(self, ctx: Context, existing_key: str):
+        if clear_aliases(existing_key):
+            await ctx.send(f"Removed all aliases of macro '{existing_key}'!")
+        else:
+            await ctx.send(f"Error: No aliases found for macro '{existing_key}'.")
+
     @commands.cooldown(3, 30, BucketType.channel)
     @commands.command(name="macros", aliases=["ml", "listmacros", "list_macros"])
     async def list_macros(self, ctx: Context):
-        macros = get_macros()
+        macros = get_macros_dict()
         if len(macros) > 0:
             macros = [f"- {key}\n" for key in sorted(macros.keys())]
             message = "📝 **Macros**:\n"
@@ -81,6 +116,19 @@ class Macro(Cog):
             await ctx.send(message)
         else:
             await ctx.send("Couldn't find any macros.")
+
+    @commands.cooldown(3, 30, BucketType.channel)
+    @commands.command(name="aliases", aliases=["listaliases", "list_aliases"])
+    async def list_aliases(self, ctx: Context, existing_key: str):
+        macros = get_macros_dict()
+        existing_key = existing_key.lower()
+        if existing_key in macros["aliases"].keys():
+            message = f"📝 **Aliases for '{existing_key}'**:\n"
+            for alias in sorted(macros["aliases"][existing_key]):
+                message += f"- {alias}\n"
+            await ctx.send(message)
+        else:
+            await ctx.send(f"Couldn't find any aliases for macro '{existing_key}'.")
 
 
 async def setup(bot):
